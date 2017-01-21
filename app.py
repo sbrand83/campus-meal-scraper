29b7fb2ab863a366ea294879e5a9a437d3d9000d
sbrand83 <stefan.brand@policystat.com>
2017-01-21 14:46:12 -0500

    added Procfile, wsgi for heroku to use

--- app.py
+++ app.py
@@ -3,7 +3,6 @@ from flask import json
 from flask import Response
 from menu_scraper import get_cafe_menu
 
-
 app = Flask(__name__)
 
 @app.route('/cafe/<date>/')
@@ -11,4 +10,5 @@ def get_todays_menu(date=""):
     data = get_cafe_menu(date)
     return Response(json.dumps(data), mimetype='application/json')
 
-app.run()
+if __name__ == "__main__":
+    app.run()
