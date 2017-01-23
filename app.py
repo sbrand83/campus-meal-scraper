7727158b8a10e6d4a83c82a13a007a5145ff0875
sbrand83 <stefan.brand@policystat.com>
2017-01-23 00:32:38 -0500

    scrapes all locations for mealtimes

--- app.py
+++ app.py
@@ -1,13 +1,13 @@
 from flask import Flask
 from flask import json
 from flask import Response
-from menu_scraper import get_cafe_menu
+from menu_scraper import get_all_locations
 
 app = Flask(__name__)
 
-@app.route('/cafe/<date>/')
+@app.route('/locations/<date>/')
 def get_todays_menu(date=""):
-    data = get_cafe_menu(date)
+    data = get_all_locations(date)
     return Response(json.dumps(data), mimetype='application/json')
 
 if __name__ == "__main__":
--- menu_scraper.py
+++ menu_scraper.py
@@ -1,50 +1,66 @@
 from bs4 import BeautifulSoup
 import json
 import re
-import sys
 from selenium import webdriver
 
-def get_cafe_menu(date):
-    url = "http://rose-hulman.cafebonappetit.com/cafe/cafe/"
+def get_all_locations(date):
+    locations = [
+        {"name": "Union Cafe", "url_path": "cafe/"},
+        {"name": "Logan's", "url_path": "logans/"},
+        {"name": "Faculty Dining Room", "url_path": "faculty-dining-room/"},
+        {"name": "C-Store", "url_path": "c-store/"},
+        {"name": "Moench", "url_path": "moench/"},
+        {"name": "Subway", "url_path": "subway/"},
+    ]
 
-    driver = webdriver.PhantomJS()
+    location_data = []
 
-    url = url + date
-    print("Requesting " + url)
-    driver.get(url)
-    print("Retrieving page...")
+    for location in locations:
+        mealtimes = get_location_meal_times(location['url_path'], date)
+        loc = {"name": location['name'], "mealTimes": mealtimes}
+        location_data.append(loc)
 
-    print("Scraping meal information...")
-    menu_page = driver.page_source
-    driver.close()
+    return location_data
+
+def get_location_meal_times(url_path, date):
+    url = "http://rose-hulman.cafebonappetit.com/cafe/"
+    url = url + url_path + date
+
+    menu_page = get_page_html(url)
     soup = BeautifulSoup(menu_page, 'html.parser')
 
     daypart_menu_panels = list()
     for i in range(1, 5):
         daypart_menu_panels.append("#panel-daypart-menu-{}".format(i))
 
-    heading_selector = ".daypart-header .panel-title"
     items_selector = "article .daypart-menu .column article"
     hours_selector = "#panel-cafe-hours .cafe-details ul"
 
     meal_panels = list()
-    headers = list()
     for meal_panel in daypart_menu_panels:
-        meal_panels.append(soup.select(meal_panel + " " + items_selector))
-        headers.append(soup.select(meal_panel + " " +  heading_selector))
+        panel = (soup.select(meal_panel + " " + items_selector))
+        if len(panel) != 0:
+            meal_panels.append(panel)
 
     # get the hours of each meal
     meal_info = get_meal_name_and_hours(hours_selector, soup)
 
     meals = []
-    for i in range(len(meal_panels)):
-        if meal_panels[i] == []:
-            continue;
-
+    print(len(meal_info))
+    for i in range(len(meal_info)):
         meal_dict = {"name": "", "hours": "", "items": []}
 
         meal_dict['hours'] = meal_info[i]['hours']
         meal_dict['name'] = meal_info[i]['name']
+
+        # may not find meal on site, if so do nothing,
+        # and return with empty items array
+        try:
+            panel = meal_panels[i]
+        except IndexError:
+            print("Number of meal hours don't match number of meal panels")
+            meals.append(meal_dict)
+            continue
 
         # get each item's name, icons, and calories
         for item in meal_panels[i]:
@@ -65,6 +81,20 @@ def get_cafe_menu(date):
         meals.append(meal_dict)
 
     return meals
+
+def get_page_html(url):
+    driver = webdriver.PhantomJS()
+
+    print("Requesting " + url)
+    driver.get(url)
+    print("Retrieving page...")
+
+    print("Scraping meal information...")
+    menu_page = driver.page_source
+    driver.close()
+    return menu_page
+
+
 
 def get_meal_name_and_hours(selector, page):
     hours_list = page.select(selector)
